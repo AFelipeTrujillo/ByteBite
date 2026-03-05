86e2748f19bd11f940cae16ce53ca9b51b8910b4
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-03-05 18:50:49 +0100

    Add Category.py class to handle the food classification

---
+++ tests/Unit/test_create_ingredient.py
@@ -0,0 +1,17 @@
+import pytest
+from uuid import uuid4
+
+from src.Domain.Entity.Ingredient import Ingredient
+from src.Domain.ValueObject.Category import Category
+
+
+async def test_create_ingredient():
+
+    ingredient1 = Ingredient(
+        id      = uuid4(),
+        name    = "Apple",
+        category    = Category.FRUITS.value
+    )
+
+    assert ingredient1.name == "Apple"
+    assert ingredient1.category == "Fruits"
