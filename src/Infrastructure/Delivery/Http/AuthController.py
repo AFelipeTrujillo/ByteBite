531e95cdd3f479651e9f87d239599d5e4edac492
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-03-06 19:14:39 +0100

    Bug fixed

--- src/Infrastructure/Delivery/Http/AuthController.py
+++ src/Infrastructure/Delivery/Http/AuthController.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from src.Application.UseCase.LoginUseCase import LoginUseCase
 from src.Infrastructure.Delivery.Http.Schemas.TokenSchema import TokenResponse
+from src.Infrastructure.DependencyInjection.MongoProviders import get_login_use_case
 
 router = APIRouter()
 
 @router.post("/login", response_model=TokenResponse)
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
-    use_case: LoginUseCase = Depends()
+    use_case: LoginUseCase = Depends(get_login_use_case)
 ):
     try:
         token_dto = await use_case.execute(
