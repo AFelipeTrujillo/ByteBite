aa57edbe05a01f07b1e0bbf5d73073e7d19382ba
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-03-06 19:08:06 +0100

    Bug fixed

--- src/Infrastructure/Delivery/Http/ShoppingListController.py
+++ src/Infrastructure/Delivery/Http/ShoppingListController.py
@@ -3,12 +3,16 @@ from typing import List
 from uuid import  UUID
 
 from src.Infrastructure.Delivery.Http.Schemas.ShoppingListSchema import ShoppingListItemResponse
+from src.Infrastructure.DependencyInjection.MongoProviders import get_generate_shopping_list_use_case
 from src.Application.UseCase.GenerateShoppingList import GenerateShoppingList
 
 router = APIRouter()
 
 @router.get("/shopping-list/{meal_plan_id}", response_model=List[ShoppingListItemResponse])
-async def generate_list(meal_plan_id: UUID, use_case: GenerateShoppingList = Depends()):
+async def generate_list(
+        meal_plan_id: UUID,
+        use_case: GenerateShoppingList = Depends(get_generate_shopping_list_use_case)
+):
 
     items_dto = await use_case.execute(meal_plan_id)
 
